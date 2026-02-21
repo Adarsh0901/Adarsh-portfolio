ad8e0d72d96e973388032dc4e07c32a87bf9d937
Adarsh <yunay@Adarshs-MacBook-Pro.local>
2026-02-22 00:28:08 +0530

    Script Changes, Contact details Updated.

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -11,6 +11,6 @@ export class ContactComponent {
     { name: 'GitHub', url: 'https://github.com/Adarsh0901', icon: 'github' },
     { name: 'LinkedIn', url: 'https://www.linkedin.com/in/adarsh-shukla-6537a31b4/', icon: 'linkedin' },
     { name: 'X', url: 'https://x.com/AdarshS25416821', icon: 'X' },
-    { name: 'Email', url: 'mailto:adarsshuklaworkspace@gmail.com', icon: 'email' },
+    { name: 'Email', url: 'mailto:adarshshuklaworkspace@gmail.com', icon: 'email' },
   ];
 }
